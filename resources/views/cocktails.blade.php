0c375e1dd9d93e18a5cedf65eb961525e77d95d1
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-07-22 15:57:11 +0200

    coctails page

--- resources/views/cocktails.blade.php
+++ resources/views/cocktails.blade.php
@@ -8,17 +8,15 @@
 <div class="container">
     <div class="row">
         <div class="image-container mt-5 mx-auto  rounded-5 bg-secondary-subtle w-75 d-flex"> 
-            @dd($cocktails)
+            
 
             <img class="w-50 " src="{{ Vite::asset('resources/img/coctails.png') }}" alt=""> 
                 
             <div class="w-50 " >
                 <h2>Cocktails name</h2>
                 <div class="d-flex justify-content-between"><p>tasso alcolemico</p> <p>prezzo</p></div>
-                <h4>Ingrediente 1</h4>
-                <h4>Ingrediente 2</h4>
-                <h4>Ingrediente 3</h4>
-                <h4>Ingrediente 4</h4>
+                <h4>Ingredienti</h4>
+                
                 
                 <h5 class="mt-5">Descrizione </h5>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam labore soluta dicta quia id vitae ea quos incidunt. Quod voluptatibus in nulla dicta vero repellat assumenda amet aspernatur error! Optio!</p>
