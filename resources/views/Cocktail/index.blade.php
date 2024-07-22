9e6352979e9425f1b8b8a1992e5d5cfea930a732
FrancescoRispoli17 <rispolifrancesco997@gmail.com>
2024-07-22 17:47:52 +0200

    [UPD] Model in Cancella button

--- resources/views/Cocktail/index.blade.php
+++ resources/views/Cocktail/index.blade.php
@@ -17,8 +17,6 @@
                 @endif
         </div>
             @foreach ($cocktails as $cocktail)
-                {{-- <li><a href="{{route('cocktails.show',$cocktail)}}">{{$cocktail->nome}}</a></li>
-                <li>€{{$cocktail->prezzo}}</li> --}}
                 <div class="col-auto my-3">
                     <div class="card" style="width: 18rem;">
                         @if (0)
@@ -29,11 +27,28 @@
                           <p class="card-text">€{{$cocktail->prezzo}}</p>
                           <a href="{{route('cocktails.show',$cocktail)}}" class="btn btn-info">Info</a>
                           <a href="{{route('cocktails.edit',$cocktail)}}" class="btn btn-primary">Modifica</a>
-                          <form action="{{route('cocktails.destroy',$cocktail)}}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">Cancella</button>
-                          </form>
+                          <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal"> Cancella</button>
+                          <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                              <div class="modal-content">
+                                <div class="modal-header">
+                                  <h1 class="modal-title fs-5" id="exampleModalLabel">Sicuro di voler procedere?</h1>
+                                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                  Non sarà più possibile recuperare il record
+                                </div>
+                                <div class="modal-footer">
+                                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Chiudi</button>
+                                  <form action="{{route('cocktails.destroy',$cocktail)}}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger">Cancella</button>
+                                  </form>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
                         </div>
                     </div>
                 </div>
