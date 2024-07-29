37eff51b72aef558b4ea299139cedfa9daedc0e9
AndVig <andrea.vigna2@gmail.com>
2024-07-29 17:08:28 +0200

    [UPD] Tabella Pivot CocktailIngredient e show

--- app/Models/CocktailIngredient.php
+++ app/Models/CocktailIngredient.php
@@ -5,11 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Ingredient extends Model
+class CocktailIngredient extends Model
 {
     use HasFactory;
 
-    public function cocktails(){
-        return $this->belongsToMany(Cocktail::class);
-    }
+    protected $table='cocktail_ingredient';
 }
